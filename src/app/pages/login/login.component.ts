db2d33c4335d1ab8da38c93ceedfac3b1818be2f
hiepnvph28430 <mrhiep165@gmail.com>
2023-05-27 00:45:06 +0700

    validate login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  form: FormGroup;
+
+
+  constructor(private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+
+    });
+  }
   credentials = {
     username: '',
     password: '',
@@ -14,5 +25,8 @@ export class LoginComponent {
   onSubmit() {
     console.log('Login submitted:', this.credentials);
     // Goi API
+    if (this.form.valid) {
+
+    }
   }
 }
